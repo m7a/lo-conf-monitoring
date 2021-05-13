82b21b75cdd0aa2db2c0963db0b753e09cceac91
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2021-05-13 21:56:52 +0200

    Allow smartctl.sh to tee the output to an external file.

--- statusscripts/smartctl.sh
+++ statusscripts/smartctl.sh
@@ -3,6 +3,15 @@
 # Copyright (c) 2021 Ma_Sys.ma.
 # For further info send an e-mail to Ma_Sys.ma@web.de.
 
+# SNYOPSIS USAGE $0 [--advanced statefile] DEVICE
+
+adv_output_file=
+if [ "$1" = "--advanced" ]; then
+	adv_output_file="$2"
+	shift 2
+fi
+
 smartoutput="$(LANG=en_US.UTF-8 smartctl -H -A "$@")"
 printf '%s\n' "$smartoutput"
+[ -z "$adv_output_file" ] || printf '%s\n' "$smartoutput" > "$adv_output_file"
 printf '%s\n' "$smartoutput" | grep -qF ": PASSED"
